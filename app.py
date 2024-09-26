5f372879ffef92e69074dd3ab22c2156203ebcc5
MaryamUmar <Mammahibrahim8@gmail.com>
2024-09-26 13:20:30 +0100

    Updated code

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import pickle
 from sklearn.cluster import KMeans
-#from sklearn.cluster import RandomForestClassifier
 import numpy as np
 import pandas as pd
+from datetime import datetime
+import os
 
 app = Flask (__name__) 
 
@@ -15,12 +16,8 @@ with open('savedModels\kmeans.plk', 'rb') as f:
 
 food_data = pd.read_csv("dataSets.csv")
 
-#food_data['Sodium_Content'] = food_data['Sodium_Content'].map({'low':1, 'high':0})
 food_data['Glycemic_Index'] = food_data['Glycemic_Index'].map({'Low':1, 'High':0})
 
-
-#def index():
-   #return render_template('index.html')
 
 @app.route('/')
 def home():
@@ -31,7 +28,7 @@ def recommendation_form():
     return render_template('index.html')
 
 @app.route('/recommend', methods=['POST'])
-def recommend_fasting():
+def recommend():
     try:
         name = (request.form['name'])
         age = int(request.form['age'])
@@ -39,6 +36,7 @@ def recommend_fasting():
         height = float(request.form['height'])
         blood_glucose_level = float(request.form['blood_glucose_level'])
         test_type = request.form['test_type']
+        allergies = request.form.getlist('allergies')
 
         bmi = weight / ((height/100) ** 2)
         if bmi < 18.5:
@@ -77,34 +75,41 @@ def recommend_fasting():
             if age >= 16:
                 if 0.0 <= blood_glucose_level <= 6.9:
                     glycemic_index_class = 0
-                    recommendation = f" Hello {name}, your fasting blood glucose level is low, you should consume high glycemic food like: "
+                    recommendation = f" Hello {name}, your random blood glucose level is low, you should consume high glycemic food like: "
                 elif 7.0 <= blood_glucose_level <= 11.0:
                     glycemic_index_class = 1
-                    recommendation = f" Hello {name}, your fasting blood glucose level is normal. Maintain diet or consume low glycemic food like: "
+                    recommendation = f" Hello {name}, your random blood glucose level is normal. Maintain diet or consume low glycemic food like: "
                 else:
                     glycemic_index_class = 1
-                    recommendation = f"Hello {name}, your fasting blood glucose level is high, you should consume low glycemic food like: "
+                    recommendation = f"Hello {name}, your random blood glucose level is high, you should consume low glycemic food like: "
             else:
                 if 0.0 <= blood_glucose_level <= 6.9:
                     glycemic_index_class = 0
-                    recommendation = f"Hello {name}, your fasting blood glucose level is low, you should consume high glycemic food like: "
+                    recommendation = f"Hello {name}, your random blood glucose level is low, you should consume high glycemic food like: "
                 elif 7.0 <= blood_glucose_level <= 11.0:
                     glycemic_index_class = 1
-                    recommendation = f"Hello {name}, your fasting blood glucose level is normal. Maintain diet or consume low glycemic food like: "
+                    recommendation = f"Hello {name}, your random blood glucose level is normal. Maintain diet or consume low glycemic food like: "
                 else:
                     glycemic_index_class = 1
-                    recommendation = f"Hello {name}, your fasting blood glucose level is high, you should consume low glycemic food like: "
+                    recommendation = f"Hello {name}, your random blood glucose level is high, you should consume low glycemic food like: "
         
         recommended_food = food_data[food_data['Glycemic_Index'] == glycemic_index_class]
+        if 'nuts' in allergies:
+            recommended_food = recommended_food[food_data['Contains_Nuts']== 'No']
+        if 'gluten' in allergies:
+            recommended_food = recommended_food[food_data['Contains_Glutten']== 'No']
+        if 'diary' in allergies:
+            recommended_food = recommended_food[food_data['Contains_Diary']== 'No']
             #print(food_data.head())
             #print (f"Glycemic Index Class: {glycemic_index_class}"} 
         
         if not recommended_food.empty:
-            food_item = recommended_food.sample(n=5)['Food_Items'].tolist()
+            food_item = recommended_food.sample(n=8)['Food_Items'].tolist()
             food_list ='<br>'.join(food_item)
             recommendation += f"{food_list}."     
         else:
             recommendation = "Sorry, {name}! No food matches your criteria."
+
 
         return render_template('result.html', recommendation=recommendation, bmi=bmi, bmi_class=bmi_class)
     except Exception as e:
